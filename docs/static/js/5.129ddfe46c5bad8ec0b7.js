a7cd7447797530c96f8fc00071ffa725d62ff96b
黄志龙 <huangzhilong@dgtle.com>
2018-03-13 13:48:36 +0800

    test 4

--- docs/static/js/5.129ddfe46c5bad8ec0b7.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([5],{LSHy:function(e,n,t){"use strict";Object.defineProperty(n,"__esModule",{value:!0});var u={name:"other",components:{HeadMenu:t("rb8n").a},computed:{},data:function(){return{}},methods:{},mounted:function(){}},r={render:function(){var e=this.$createElement,n=this._self._c||e;return n("div",[n("Head-Menu")],1)},staticRenderFns:[]},a=t("U5Ua")(u,r,!1,null,null,null);n.default=a.exports}});
-//# sourceMappingURL=5.129ddfe46c5bad8ec0b7.js.map
